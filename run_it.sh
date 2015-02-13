c95f37ccb72ae5d0bd3b51b7010d3f56c8fd9b21
Lachlan Mackenzie <lachlan.mackenzie@codethink.co.uk>
2015-02-13 11:06:28 +0000

    Add script to run weston

--- run_it.sh
+++ run_it.sh
@@ -1,2 +1,6 @@
-export XDG_RUNTIME_DIR=/tmp/adit-test2-dir
+export XDG_RUNTIME_DIR=/tmp/adit-test2-runtime-dir
+if ! test -d "${XDG_RUNTIME_DIR}"; then
+    mkdir "${XDG_RUNTIME_DIR}"
+    chmod 0700 "${XDG_RUNTIME_DIR}"
+fi
 weston-launch -- --backend=fbdev-backend.so > error.txt 2>&1
