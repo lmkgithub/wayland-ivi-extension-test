57cdb6fbf86e7f5b1d636814fb4284a31dd56b50
Lachlan Mackenzie <lachlan.mackenzie@codethink.co.uk>
2015-03-03 16:27:44 +0000

    Add functions to allocate layers and surfaces

--- ivi-layermanagement-api/test/TestBase.h
+++ ivi-layermanagement-api/test/TestBase.h
@@ -2,12 +2,18 @@
 #include "wayland-client.h"
 #include <vector>
 
+extern "C" {
+    #include "ilm_client.h"
+    #include "ilm_control.h"
+}
+
 class TestBase
 {
 public:
     TestBase();
     virtual ~TestBase();
-
+    t_ilm_surface getSurface();
+    t_ilm_layer   getLayer();
 protected:
     std::vector<wl_surface *> wlSurfaces;
     wl_display*    wlDisplay;
@@ -15,4 +21,6 @@ protected:
 private:
     wl_registry*   wlRegistry;
     wl_compositor* wlCompositor;
+    t_ilm_surface  nextSurfaceId;
+    t_ilm_layer    nextLayerId;
 };
