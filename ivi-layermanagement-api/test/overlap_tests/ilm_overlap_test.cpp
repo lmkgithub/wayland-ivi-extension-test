0eb7d4feb0eff2efbb5948f619203e1801f45874
Lachlan Mackenzie <lachlan.mackenzie@codethink.co.uk>
2015-03-27 18:11:05 +0000

    Add ilm_overlapGetScreenIDs

--- ivi-layermanagement-api/test/overlap_tests/ilm_overlap_test.cpp
+++ ivi-layermanagement-api/test/overlap_tests/ilm_overlap_test.cpp
@@ -160,6 +160,8 @@ public:
             vectorOfTestNames.push_back("IlmOverlapTest_ilm_overlapSurfaceSetSourceRectangle");
             vectorOfTests.push_back(&IlmOverlapTest::IlmOverlapTest_ilm_overlapGetPropertiesOfLayer);
             vectorOfTestNames.push_back("IlmOverlapTest_ilm_overlapGetPropertiesOfLayer");
+            vectorOfTests.push_back(&IlmOverlapTest::IlmOverlapTest_ilm_overlapGetScreenIDs);
+            vectorOfTestNames.push_back("IlmOverlapTest_ilm_overlapGetScreenIDs");
     }
 
     void TearDown()
@@ -872,6 +874,23 @@ public:
              EXPECT_EQ(returnValue.visibility,
                        layers_allocated[i].layerProperties.visibility);
         }
+    }
+
+    void IlmOverlapTest_ilm_overlapGetScreenIDs()
+    {
+        std::cout << "Running: " << __FUNCTION__ << std::endl;
+
+        t_ilm_uint numberOfScreens = 0;
+        t_ilm_uint* screenIDs;
+
+        // Try to get screen IDs using valid pointer for numberOfScreens
+        ASSERT_EQ(ILM_SUCCESS, ilm_getScreenIDs(&numberOfScreens, &screenIDs));
+
+        v_screenID.clear();
+        v_screenID.assign(screenIDs, screenIDs + numberOfScreens);
+        free(screenIDs);
+
+        EXPECT_TRUE(numberOfScreens > 0);
     }
 };
 
